dd05eea00bdade213c397a50e0fc0bcae2dbc82e
sergiosacristan <sergio.sacristan@glovoapp.com>
2020-03-27 10:39:19 +0100

    parallel DB test execution

---
+++ sql/V0.1__initial_schema.sql
@@ -0,0 +1,6 @@
+
+CREATE TABLE `testTable` (
+  `type` varchar(31) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
--- src/test/java/com/example/cucumber/gradle/parallel/demo/CreateContextHook.java
+++ src/test/java/com/example/cucumber/gradle/parallel/demo/CreateContextHook.java
@@ -3,10 +3,12 @@ package com.example.cucumber.gradle.parallel.demo;
 import io.cucumber.java.Before;
 import org.springframework.boot.test.context.SpringBootContextLoader;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration(classes = DemoApplication.class, loader = SpringBootContextLoader.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@ContextConfiguration(classes = DemoApplication.class, loader = SpringBootContextLoader.class)
+//@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@SpringBootTest
 public class CreateContextHook {
     @Before
     public void setupCucumberSpringContext() {
